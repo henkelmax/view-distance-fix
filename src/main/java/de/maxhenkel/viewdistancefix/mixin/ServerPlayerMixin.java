a848912417eda5169bc4166f867faf82ec7145cf
Max Henkel <max@maxhenkel.de>
2022-12-01 16:56:11 +0100

    Prepare for 1.19.3

--- src/main/java/de/maxhenkel/viewdistancefix/mixin/ServerPlayerMixin.java
+++ src/main/java/de/maxhenkel/viewdistancefix/mixin/ServerPlayerMixin.java
@@ -8,9 +8,7 @@ import net.minecraft.network.protocol.game.ServerboundClientInformationPacket;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.player.ProfilePublicKey;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,8 +21,8 @@ public abstract class ServerPlayerMixin extends Player {
     @Shadow
     public ServerGamePacketListenerImpl connection;
 
-    public ServerPlayerMixin(Level level, BlockPos blockPos, float f, GameProfile gameProfile, @Nullable ProfilePublicKey profilePublicKey) {
-        super(level, blockPos, f, gameProfile, profilePublicKey);
+    public ServerPlayerMixin(Level level, BlockPos blockPos, float f, GameProfile gameProfile) {
+        super(level, blockPos, f, gameProfile);
     }
 
     @Inject(method = "updateOptions", at = @At("HEAD"))
